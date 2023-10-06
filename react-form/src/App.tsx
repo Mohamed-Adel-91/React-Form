824a6abfcc2c6d7f337137eb1acb9099f134d3b1
Mohamed Adel <mohamed101291@gmail.com>
2023-10-06 19:56:17 +0300

    Handel Delete Button

--- react-form/src/App.tsx
+++ react-form/src/App.tsx
@@ -1,34 +1,36 @@
+import { useState } from "react"
 import ExpenseList from "./components/expense-tracker/ExpenseList"
-const expenses = [
-  {
-  id: 1,
-  description: 'Groceries',
-  amount: 30,
-  category: 'Util',
-},
-{
-  id: 2,
-  description: 'Groceries',
-  amount: 50,
-  category: 'Util',
-},
-{
-  id: 3,
-  description: 'Groceries',
-  amount: 60,
-  category: 'Util',
-},
-{
-  id: 4,
-  description: 'Groceries',
-  amount: 130,
-  category: 'Util',
-}
-]
+
 const App = () => {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: 'Groceries',
+      amount: 30,
+      category: 'Util',
+    },
+    {
+      id: 2,
+      description: 'Groceries',
+      amount: 50,
+      category: 'Util',
+    },
+    {
+      id: 3,
+      description: 'Groceries',
+      amount: 60,
+      category: 'Util',
+    },
+    {
+      id: 4,
+      description: 'Groceries',
+      amount: 130,
+      category: 'Util',
+    },
+    ]);
   return (
     <div>
-      <ExpenseList expenses={expenses} onDelete={(id) => console.log('Deleted', id)}/>
+      <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))}/>
     </div>
   )
 }
